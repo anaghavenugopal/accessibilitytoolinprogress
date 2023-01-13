8bd3a8ddb77197e3f8ea3a11cffe7bec17d60fc4
Anagha Venugopal <105713056+anaghavenugopal@users.noreply.github.com>
2023-01-13 11:18:09 +0100

    Update interactivewebapp_code.py

--- interactivewebapp_code.py
+++ interactivewebapp_code.py
@@ -1,4 +1,4 @@
 import streamlit as st
+st.title("Interactive WebApp")
 st.map()
-Interactive WebApp
 df_uploaded = st.sidebar.file_uploader('Select the coordinates in map by clicking the arrow icon')
