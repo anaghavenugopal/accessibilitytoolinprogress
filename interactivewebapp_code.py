d1ed9606823010a028ef14bb15cf5cd837ad9eb0
Anagha Venugopal <105713056+anaghavenugopal@users.noreply.github.com>
2023-01-13 11:33:18 +0100

    Update interactivewebapp_code.py

--- interactivewebapp_code.py
+++ interactivewebapp_code.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title("Accessibility Analysis Tool")
 st.map()
 df_uploaded = st.sidebar.file_uploader('Select the coordinates in map by clicking the arrow icon')
+st.sidebar.multiselect('Travel Time',['5 minutes','10 minutes','15 minutes','30 minutes','45 minutes','60 minutes'])
+st.sidebar.button("Download Report")
